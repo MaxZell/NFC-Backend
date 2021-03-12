3000142307045b39481c46e2a068ccb9f48ef41d
mzelensky <maxim.zelensky@soxes.ch>
2021-03-12 15:55:21 +0100

    port for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 9500;;
+const port = process.env.PORT || 5000;;
 
 app.listen(port, () => {
     console.log('server running on port: ' + port);
