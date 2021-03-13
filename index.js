ce2272588f6fd1fffe7aeec5e65c81c0469854c8
mzelensky <maxim.zelensky@soxes.ch>
2021-03-13 22:29:54 +0100

    add parser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 ﻿const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;;
+const port = process.env.PORT || 5000;
+
+// app.use(express.json());
+app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log('server running on port: ' + port);
