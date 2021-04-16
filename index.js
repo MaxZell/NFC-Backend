7d4e2a99bf50a97ffb976f1ba906b02db9eb0fd3
mzelensky <maxim.zelensky@soxes.ch>
2021-04-17 00:28:05 +0200

    mqtt added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const mqtt = require('mqtt')
+const MqttClient  = mqtt.connect('mqtt://cloud.tbz.ch')
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,6 +20,9 @@ app.get('/', function(res) {
     res.sendFile(path.join(__dirname, 'interface/', 'index.html'));
 });
 
+/**
+ * HTTP
+*/
 app.get('/ajax/get-data', function(req, res) {
     data = "some data";
     res.send(`data from server: ${data}`);
@@ -32,9 +38,9 @@ app.post('/ajax/save-data', function(req, res) {
     const pw = process.env.Mongo_m242;
     const uri = `mongodb+srv://m242:${pw}@cluster0.9rupy.mongodb.net/m242?retryWrites=true&w=majority`;
     const client = new MongoClient(encodeURI(uri), { useNewUrlParser: true, useUnifiedTopology: true });
+    
     client.connect(err => {
         console.log('-------', err)
-        
         console.log("myhash: " + hash)
         var myobj = { uid: hash};
         client.db("m242").collection("cards").insertOne(myobj, function(err, res) {
@@ -49,3 +55,61 @@ app.post('/ajax/save-data', function(req, res) {
 app.post('/ajax/login', function(req, res) {
     res.send(`data from server: ${req.body.uid}`);
 });
+
+/**
+ *  @todo: create http get for node-red
+ * + parse function
+*/
+
+/**
+ * MQTT
+*/
+const topic_id = "8x3ebv3f4w3EUchR";
+
+MqttClient.on('connect', function () {
+    MqttClient.subscribe(`${topic_id}/sensor`, function (err) {
+      if (!err) {
+          console.log("answer sended");
+          MqttClient.publish(`${topic_id}/answer`, 'hi from Max =>connected')
+      }
+    })
+})
+
+MqttClient.on('connect', () => {
+    MqttClient.subscribe(`${topic_id}/sensor`)
+    MqttClient.subscribe(`${topic_id}/temperature`)
+    MqttClient.subscribe(`${topic_id}/login`)
+})
+
+MqttClient.on('message', function (topic, message) {
+    if(topic === `${topic_id}/sensor`) {
+        console.log(message.toString())
+        try {
+            let preparedString = message.toString().replace(/'/gi, '"')
+            console.log(preparedString);
+            let mqttJson = JSON.parse(JSON.parse(JSON.stringify(preparedString.toString())))
+            console.log(`time: ${mqttJson.uid}\ntime: ${mqttJson.time}\ntemperature: ${mqttJson.temperature}`);
+          } catch(err) {
+            console.error(err)
+        }
+    }else if(topic === `${topic_id}/temperature`){
+        // const pw = process.env.Mongo_m242;
+        // const uri = `mongodb+srv://m242:${pw}@cluster0.9rupy.mongodb.net/m242?retryWrites=true&w=majority`;
+        // const client = new MongoClient(encodeURI(uri), { useNewUrlParser: true, useUnifiedTopology: true });
+
+        // client.connect(err => {
+        //     console.log('-------', err)
+        //     client.connect(err => {
+        //         console.log('-------', err)
+        //         var myobj = {"uid": "test2"};
+        //         client.db("m242").collection("cards").insertOne(myobj, function(err, res) {
+        //             if (err) throw err;
+        //                 console.log("UID saved");  
+        //         })
+        //     });
+        // });
+    }else if(topic === `${topic_id}/login`){
+        console.log(message.toString())
+    }
+    // MqttClient.end()
+})
