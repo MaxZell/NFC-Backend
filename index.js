dbbdbe9ae57a51f95b7ec78d175500f41d21d4a3
mzelensky <maxim.zelensky@soxes.ch>
2021-04-23 15:03:02 +0200

    mqtt added + login/register

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const path = require('path');
+const bcrypt = require('bcrypt');
+
+const mqtt = require('mqtt')
+const MqttClient  = mqtt.connect('mqtt://cloud.tbz.ch')
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// const pw = process.env.Mongo_m242;
+const pw = "mypass";
+const uri = `mongodb+srv://m242:${pw}@cluster0.9rupy.mongodb.net/m242?retryWrites=true&w=majority`;
+const client = new MongoClient(encodeURI(uri), { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.use(bodyParser.json());
 
@@ -29,12 +39,8 @@ app.post('/ajax/save-data', function(req, res) {
         fetch("http://localhost:5000/ajax/save-data", {"method": "POST", "body": {"uid":"test"}}).then(r => console.log(r))
     */
     let hash = req.body.uid;
-    const pw = process.env.Mongo_m242;
-    const uri = `mongodb+srv://m242:${pw}@cluster0.9rupy.mongodb.net/m242?retryWrites=true&w=majority`;
-    const client = new MongoClient(encodeURI(uri), { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         console.log('-------', err)
-        
         console.log("myhash: " + hash)
         var myobj = { uid: hash};
         client.db("m242").collection("cards").insertOne(myobj, function(err, res) {
@@ -46,6 +52,44 @@ app.post('/ajax/save-data', function(req, res) {
     });
 });
 
+const topic_id = "8x3ebv3f4w3EUchR";
+
 app.post('/ajax/login', function(req, res) {
-    res.send(`data from server: ${req.body.uid}`);
+    client.connect(err => {
+        console.log('-------', err)
+        let inpt = req.body.uid;
+        let query = { "uid" : inpt}
+        client.db("m242").collection("cards").findOne(query, function(err, result) {
+            // if (err) throw err;
+            if(result){
+                MqttClient.publish(`${topic_id}/answer`, `exist`)
+                res.send(`data from server: ${result.uid}`);
+            }else if(result == null){
+                MqttClient.publish(`${topic_id}/answer`, `notexist`)
+            }
+            console.log(result);
+        });
+    });
+    
+    // const salt = bcrypt.genSaltSync(10);
+    // let hash = bcrypt.hashSync("Hello", 10);
+    // bcrypt.compareSync("Hello", hash);
+    // console.log(hash);
+    // res.send(`data from server: ${hash}`);
+});
+
+app.post('/ajax/register', function(req, res) {
+    let inpt = req.body.uid;
+    let hash = bcrypt.hashSync(inpt, 10);
+    client.connect(err => {
+        console.log('-------', err)
+        client.connect(err => {
+            console.log('-------', err)
+            var myobj = {"uid": inpt, "hash": hash};
+            client.db("m242").collection("cards").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                    MqttClient.publish(`${topic_id}/answer`, `UID saved`)
+            })
+        });
+    });
 });
